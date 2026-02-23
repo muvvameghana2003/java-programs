40c305584040d88c4fc2f3bf1ecd66dd27f24afe
Muvva Meghana <meghanamuvva184@gmail.com>
2026-02-23 12:09:51 +0530

    Add swap number program

---
+++ swap.java
@@ -0,0 +1,32 @@
+//using the third variable.
+/*import java.util.*;
+class swap
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=a;
+		a=b;
+		b=c;
+		System.out.print(a);
+		System.out.print(b);
+	}
+}*/
+
+//without using third variable.
+import java.util.*;
+class swap1
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.print(a+" "+b);
+	}
+}
